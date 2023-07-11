b851843d31defb8c84be81e9350132c404ad6e81
“maria <marislaime1@gmail.com>
2023-07-11 18:26:39 -0300

    modificated

--- src/components/ResidentsCard.jsx
+++ src/components/ResidentsCard.jsx
@@ -3,43 +3,7 @@ import axios from "axios"
 import "./styles/ResidentsCard.css"
 
 const ResidentsCard = ({url}) => {
-    const [character, setCharacter] = useState()
-    useEffect(() => {
-    axios
-    .get(url)
-    .then(resp=>setCharacter(resp.data))
-    .catch(error=>console.error(error))
-    }, [])
-
-    
-  return (
-    <article className="resident">
-        <header className="resident__header">
-            <img className="resident__img" src={character?.image} alt="img" />
-            <div className="resident__status">
-                <div className={`resident__status-circle ${character?.status}`}></div>
-                <span className="resident__status-value">{character?.status}</span>
-            </div>
-        </header>
-        <section className="resident__body">
-            <h3 className="resident__name">{character?.name}</h3>
-            <hr className="resident__line"/>
-            <ul className="resident__list">
-                <li className="resident__item">
-                    <span className="resident__label">Specie</span>
-                    <span className="resident__item-value">{character?.species}</span>
-                </li>
-                <li className="resident__item">
-                    <span className="resident__label">Origin</span>
-                    <span className="resident__item-value">{character?.origin.name}</span>
-                </li>
-                <li className="resident__item">
-                    <span className="resident__label">Eppisodes where appear</span>
-                    <span className="resident__item-value">{character?.episode.length}</span>
-                </li>
-            </ul>
-        </section>
-    </article>
+  
 
   )
 }
