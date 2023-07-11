44e28032808e8bedfc51f8237359f327049ae93f
“maria <marislaime1@gmail.com>
2023-07-11 08:42:39 -0300

    loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import "./styles/Loader.css"
+const Loader =()=>{
+  return(
+    
+    <div className="loader">
+         <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>    
+    </div>
+    </div>
+  )
+}
+export default Loader
+
