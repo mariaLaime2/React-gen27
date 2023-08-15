3fa14f97344b2f54c242dbdcec2926f876b8e8c4
“maria <marislaime1@gmail.com>
2023-08-15 04:42:33 -0300

    navegation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import "./App.css";
+import Sidernav from "./components/Sidernav";
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-    
+    <Sidernav/>
     </>
   )
 }
