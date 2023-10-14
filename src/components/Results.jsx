763700ceba16e83f0ea18ffb17a88ce7b062cdff
“maria <marislaime1@gmail.com>
2023-10-14 18:36:34 -0300

    results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,6 @@
 export const Results = ({ score, questionsFiltered, onReset }) => {
   return (
-    <div className="flex flex-col justify-evenly items-center shadow-xl rounded-lg p-10 w-[400px] h-[600px] ml-2 mr-2 mt-10 border border-solid gap-5">
+    <div className="flex flex-col justify-evenly items-center shadow-xl rounded-lg p-10 w-[400px] h-[600px] ml-2 mr-2 mt-10 mb-10 border border-solid gap-5">
       <h1 className="text-3xl font-bold">Resultados</h1>
       <span className="text-8xl">🏆</span>
       <div className="flex flex-col gap-5 text-center text-lg font-bold">
