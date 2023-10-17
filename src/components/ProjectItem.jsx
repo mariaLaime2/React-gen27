4a5c0bcc76871310ce51dcd5b127c56cba20d16e
“maria <marislaime1@gmail.com>
2023-10-16 23:01:13 -0300

    Items

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,6 @@
 import { GrGithub } from "react-icons/gr";
 
-const ProjectItem = ({ img, url, title, urlG,tech }) => {
+const ProjectItem = ({ img, url, title, urlG }) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#cc5993]">
       <img src={img} alt="" className="rounded-xl group-hover:opacity-10" />
@@ -8,9 +8,6 @@ const ProjectItem = ({ img, url, title, urlG,tech }) => {
         <h3 className="text-xl font-bold text-white tracking-wider text-center">
           {title}
         </h3>
-        <h4 className="text-sm font-bold text-white tracking-wider text-center">
-          {tech}
-        </h4>
         <section className="flex flex-col gap-2">
           <a href={url} target="blanck">
             <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-sm">
